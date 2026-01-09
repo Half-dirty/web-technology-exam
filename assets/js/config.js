e9301b1f862685737408217825ba01a1a699773e
Никита <nikitosicshe@gmail.com>
2026-01-09 20:39:35 +0400

    Базовая конфигурация API

---
+++ assets/js/config.js
@@ -0,0 +1,30 @@
+window.APP_CONFIG = {
+  apiBaseUrlPrimary: "https://exam-api-courses.std-900.ist.mospolytech.ru",
+  apiBaseUrlFallback: "http://exam-api-courses.std-900.ist.mospolytech.ru",
+
+  apiKey: "cf9d9c3a-27bd-422b-a940-579d912a1e89",
+
+  pagination: {
+    coursesPerPage: 10,
+    ordersPerPage: 5
+  },
+
+  ui: {
+    toastAutoHideMs: 5000
+  },
+
+  pricing: {
+    weekendMultiplier: 1.5,
+    morningSurcharge: 400,
+    eveningSurcharge: 1000,
+    intensiveThresholdWeekHours: 20,
+    intensiveMultiplier: 1.2,
+    excursionsMultiplier: 1.25,
+    interactiveMultiplier: 1.5,
+    supplementaryPerStudent: 2000,
+    personalizedPerWeek: 1500,
+    assessmentFee: 300,
+    earlyDiscount: 0.10,
+    groupDiscount: 0.15
+  }
+};
